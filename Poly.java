96230b2d5ce87b02655ec782521a7e15c298a544
RJN95 <111572359+Reginold1995@users.noreply.github.com>
2022-09-25 20:55:04 +0530

    Add files via upload

---
+++ Poly.java
@@ -0,0 +1,26 @@
+class Poly
+{
+    public static void main(String args[])
+    {
+        Poly obj=new Poly();
+        obj.add();
+    }
+       
+        void add()
+        {
+            int c=a+b;
+            System.out.println(c);
+        }
+        void add(int a, int b, int c)
+        {
+            int d=a+b+c;
+             System.out.println(d);
+        }
+        void add()
+        {
+            float c=a-b;
+            System.out.println(c);
+        }
+         
+      
+} 
